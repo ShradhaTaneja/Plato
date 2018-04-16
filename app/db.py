dd2a54b478f0d306ab62838b2610da137dc965e0
Shradha Taneja <shradha.taneja@gmail.com>
2018-04-16 10:47:16 -0400

    correct database file

--- app/db.py
+++ app/db.py
@@ -1,9 +1,7 @@
+import app.config as config
 import MySQLdb
-import config
 
-class MySql:
-    def __init__(self):
-        pass
-    def get_conn(self):
-        conn = MySQLdb.connect(host = config.MYSQL_CREDS['HOST'], user = config.MYSQL_CREDS['USER'], db = config.MYSQL_CREDS['DATABASE'], passwd = config.MYSQL_CREDS['PASSWORD'])
-        return conn
+def get_mysql_conn():
+    conn = MySQLdb.connect(host = config.MYSQL_CREDS['HOST'], user = config.MYSQL_CREDS['USER'], db = config.MYSQL_CREDS['DATABASE'], passwd = config.MYSQL_CREDS['PASSWORD'])
+    return conn
+
